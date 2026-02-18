84b1a9b34eeb397c031156552c62bb7cd7354f7c
Stephanie Fong <fong.s@ufl.edu>
2026-02-18 14:33:49 -0500

    Created base template for main event page

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./routes/Landing";
 import { UpcomingEventsPage } from "./routes/UpcomingEvents";
+import { EventsPage } from "./routes/Events";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/events" element={<EventsPage />} />
         <Route path="/upcoming" element={<UpcomingEventsPage />} />
       </Routes>
     </BrowserRouter>
