15c2fe290fb70a79c534af64655d1cb5cb47a193
Stephanie Fong <fong.s@ufl.edu>
2026-04-13 12:18:26 -0400

    converted native css styling to tailwind

--- src/client/components/card.tsx
+++ src/client/components/card.tsx
@@ -1,6 +1,6 @@
 // src/components/Card.jsx
-import React from 'react';
-import '../index.css'; // Import the CSS file
+import React from "react";
+import "../index.css"; // Import the CSS file
 
 interface CardProps {
   title: string;
@@ -12,14 +12,22 @@ interface CardProps {
 
 const Card = ({ title, subtitle, content, imageUrl, linkUrl }: CardProps) => {
   return (
-    <div className="card">
-      {imageUrl && <img src={imageUrl} alt={title} className="card-image" />}
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        {subtitle && <h4 className="card-subtitle">{subtitle}</h4>}
-        <p className="card-text">{content}</p>
+    <div className="border border-white rounded w-[300px] m-4 overflow-hidden transition delay-1000 hover:translate-y-[-5px]">
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={title}
+          className="w-full h-[180px] object-cover"
+        />
+      )}
+      <div className="p-4">
+        <h2 className="text-xl mb-2">{title}</h2>
+        {subtitle && (
+          <h4 className="text-sm text-customGray mb-4">{subtitle}</h4>
+        )}
+        <p className="text-base mb-4">{content}</p>
         {linkUrl && (
-          <a href={linkUrl} className="card-link">
+          <a href={linkUrl} className="text-customDarkBlue no-underline">
             Learn More
           </a>
         )}
